979f5a88c092dd8032b72f936cce7686f10db27d
rafliafriza12 <rafliafriza90@gmail.com>
2024-03-17 04:41:08 +0700

    feat: add seed random number code and add data random number in txt file

---
+++ 2208107010028_Simple_sorting.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 1000000
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
---
+++ seedRandomNumber.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 1000000
+
+// void bubbleSort(unsigned * data);
+void seedRandomNumber(int data[], int n);
+
+int main(int argc, char *argv[])
+{
+    FILE * fp;
+    fp = fopen("data_random.txt", "w");
+
+    if(fp == NULL){
+        printf("error opening file");
+        exit(0);
+    }
+
+    int data[SIZE];
+
+    seedRandomNumber(data, SIZE);
+
+    for (int i = 0; i < sizeof(data) / sizeof(data[0]); i++)
+    {
+        fprintf(fp,"%d\n",data[i]);
+        printf("data : %d tertuliske file\n", data[i]);
+    }
+
+    printf("Success\n");
+    fclose(fp);
+    return 0;
+}
+
+void seedRandomNumber(int data[], int n)
+{
+    
+    int i = 0;
+
+    while (i < n)
+    {   
+        int assignment = 1;
+        int check = 1;
+        int randomNumber = (rand() % 1000000) + 1;
+        int j = 0;
+
+        if (i != 0) {
+            while (check && j < i) {
+                if (data[j] == randomNumber) {
+                    check = 0;
+                    assignment = 0;
+                }
+                j++;
+            }
+        }
+
+        if (assignment) {
+            printf("haloo %d\n",i);
+            data[i] = randomNumber;
+            i++;
+        }
+    }
+}
