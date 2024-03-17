87797e05ce6490d59536e258926264c6587bb300
rafliafriza12 <rafliafriza90@gmail.com>
2024-03-17 21:53:12 +0700

    done: all the program is done

--- 2208107010028_Simple_sorting.c
+++ 2208107010028_Simple_sorting.c
@@ -5,6 +5,148 @@
 
 int main(int argc, char *argv[])
 {
-    
+    FILE *fp;
+    FILE *sorting;
+    FILE *sortTime;
+    int arr[SIZE];
+    int batas = 100000;
+    sortTime = fopen("waktu_sorting.txt", "w");
+    sorting = fopen("data_sorting.txt", "w");
+    fp = fopen("data_random.txt", "r");
+
+    if (fp == NULL || sorting == NULL || sortTime == NULL)
+    {
+        printf("error open file");
+        exit(0);
+    }
+
+    for (int i = 1; i <= batas; i++)
+    {
+        fscanf(fp, "%d\n", &arr[i - 1]);
+        if (i % batas == 0)
+        {
+            printf("bubble sort start\n");
+            clock_t start = clock();
+            for (int j = 0; j < batas - 1; j++)
+            {
+                for (int k = 0; k < batas - j - 1; k++)
+                {
+                    if (arr[k] > arr[k + 1])
+                    {
+                        int temp = arr[k];
+                        arr[k] = arr[k + 1];
+                        arr[k + 1] = temp;
+                    }
+                }
+            }
+            clock_t end = clock();
+            double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+            printf("Waktu eksekusi bubblesort (jlh data: %d): %f detik\n",batas,cpu_time_used);
+
+            fprintf(sortTime,"Waktu eksekusi bubblesort (jlh data: %d): %f detik\n",batas,cpu_time_used);
+
+            if (batas == SIZE)
+            {
+                for (int j = 0; j < batas; j++)
+                {
+                    fprintf(sorting, "%d\n", arr[j]);
+                }
+            }
+
+            i = 0;
+            fseek(fp, SEEK_SET, 0);
+            if (batas == SIZE)
+            {
+                break;
+            }
+            batas += 100000;
+            printf("\n");
+        }
+    }
+
+    batas = 100000;
+
+    for (int i = 1; i <= batas; i++)
+    {
+        fscanf(fp, "%d\n", &arr[i - 1]);
+        if (i % batas == 0)
+        {
+            printf("selection sort start\n");
+            clock_t start = clock();
+
+            for (int j = 0; j < batas - 1; j++)
+            {
+                int min_idx = j;
+                for (int k = j + 1; k < batas; k++)
+                {
+                    if (arr[k] < arr[min_idx])
+                    {
+                        min_idx = k;
+                    }
+                }
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[j];
+                arr[j] = temp;
+            }
+
+            clock_t end = clock();
+            double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+            printf("Waktu eksekusi selection sort (jlh data: %d): %f detik\n", batas, cpu_time_used);
+            fprintf(sortTime, "Waktu eksekusi selection sort (jlh data: %d): %f detik\n", batas, cpu_time_used);
+            i = 0;
+            fseek(fp, SEEK_SET, 0);
+            if (batas == SIZE)
+            {
+                break;
+            }
+            batas += 100000;
+            printf("\n");
+        }
+    }
+
+    batas = 100000;
+
+    for (int i = 1; i <= batas; i++)
+    {
+        fscanf(fp, "%d\n", &arr[i - 1]);
+        if (i % batas == 0)
+        {
+            printf("insertion sort start\n");
+            clock_t start = clock();
+
+            for (int j = 1; j < batas; j++)
+            {
+                int key = arr[j];
+                int k = j - 1;
+                // Pindahkan elemen yang lebih besar dari key satu posisi ke depan
+                // dari posisi mereka saat ini
+                while (k >= 0 && arr[k] > key)
+                {
+                    arr[k + 1] = arr[k];
+                    k = k - 1;
+                }
+                arr[k + 1] = key;
+            }
+
+            clock_t end = clock();
+            double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+            printf("Waktu eksekusi insertion sort (jlh data: %d): %f detik\n", batas, cpu_time_used);
+
+            fprintf(sortTime, "Waktu eksekusi insertion sort (jlh data: %d): %f detik\n", batas, cpu_time_used);
+
+            i = 0;
+            fseek(fp, SEEK_SET, 0);
+            if (batas == SIZE)
+            {
+                break;
+            }
+            batas += 100000;
+            printf("\n");
+        }
+    }
+
+    fclose(fp);
+    fclose(sorting);
+    fclose(sortTime);
     return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Data-Structure
 This is my data structure task repository
+
+# Run the code
+Pertama-tama yang harus dijalankan adalah file seedRandomNumber.c untuk menghasilkan nomor acak dari 1 sampai 1juta, dijalankan dengan cara di compile terlebih dahulu lalu jalankan file executablenya, file ini hanya perlu dijalankan sekali, JIKA DI DALAM REPOSITORY SAYA SUDAH ADA FILE YANG BERNAMA "data_random.txt" MAKA FILE INI TIDAK PERLU DIJALANKAN LAGI
+
+> gcc seedRandomnumber.c -o seedRandomNumber
+> ./seedRandomNumber
+
+ketika file seedRandomNumber.c sudah dijalankan, selanjutnya menjalankan file 2208107010028_Simple_sorting.c dengan cara di compile terlebih dahulu dan menjalankan file executablenya
+
+> gcc 2208107010028_Simple_sorting.c -o main
+> ./main
+
+# Note
+=> angka acak akan tersimpan secara otomatis di file data_random.txt
+
+=> angka yang telah tersorting akan otomatis tersimpan ketika program selesai     dijalankan
+
+=> waktu yang ditempuh setiap sorting akan otomatis tercatat di file waktu_sorting.txt
+
+# My function
+Funsi yang saya gunakan di file seedRandomNumber.c hanya fungsi main dan fungsi seedRandomNumber, yang dimana fungsi seedRandomNumber berguna untuk menghasilkan nilai acak dan memasukkannya ke sebuah array, nilai acak yang dihasilkan pasti akan berbeda dan tidak ada yang sama, lalu fungsi main berguna untuk menjalankan program, memanggil fungsi seedRandomNumber, dan menuliskan angka acak yang sudah disimpan di array ke dalam sebuah file txt.
+
+Fungsi yang saya gunakan di file 2208107010028_Simple_sorting.c hanya fungsi main, isi dari fungsi main yaitu, membuka file yang diperlukan, membaca file untuk memasukkan data random ke array untuk di sorting, dan mencatat waktu setiap sorting yang dilakukan
